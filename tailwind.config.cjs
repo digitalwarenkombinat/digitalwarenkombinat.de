f50298b01fd5f0b40bbc2e138ca08e1090bb570a
Gerd Müller <g.mueller@ewerk.com>
2022-03-15 11:32:46 +0100

    Add tailwind/postcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    extend: {
+      colors: {
+        primary: '#dd8d0e',
+        secondary: '#009599',
+        tertiary: '#33006c',
+        border: '#472f12',
+        background: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+};

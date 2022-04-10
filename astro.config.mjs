47818f643ac3923434a6e21af11461307a9eda24
Gerd Müller <gerdesque@posteo.org>
2022-04-10 18:12:41 +0200

    Adjust astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
 	integrations: [
     preact(),
   ],
+  site: 'https://digitalwarenkombinat.github.io/digitalwarenkombinat.de/',
+  base: '/digitalwarenkombinat.de'
 });

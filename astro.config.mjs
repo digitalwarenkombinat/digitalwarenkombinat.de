1d9df188f9a9baca10b64755e82880a598d2d42f
Gerd Müller <gerdesque@posteo.org>
2024-04-20 11:47:15 +0200

    🧹 chore(app): Adjust global config / dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,36 @@
-import preact from '@astrojs/preact';
-import { defineConfig } from 'astro/config'
-import AstroPWA from '@vite-pwa/astro'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import rehypeExternalLinks from 'rehype-external-links'
+import { defaultLocale, locales } from './src/i18n/i18n';
+import { site } from './src/consts';
 
+const sitemapLocales = Object.fromEntries(locales.map((_, i) => [locales[i], locales[i]])) // Create an object with keys and values based on locales
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact(),
-    AstroPWA({
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Digitalwarenkombinat',
-        short_name: 'Diwako',
-        description: 'Digitalagentur für Museen, Archive, Bibliotheken und Galerien. Webdesign, Entwicklung und Beratung für digitalisierte Sammlungen.',
-        theme_color: '#dd8d0e',
-        icons: [
-          {
-            src: "pwa-64x64.png",
-            sizes: "64x64",
-            type: "image/png"
-          },
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable"
-          }
-        ]
-      }
-    })
-  ]
-})
+	site: site,
+	integrations: [
+		mdx(),
+		sitemap({
+			filter: (page) => page.secret !== true,
+			i18n: {
+				defaultLocale: defaultLocale,
+				locales: sitemapLocales,
+			}
+		})
+	],
+	i18n: {
+		defaultLocale: defaultLocale,
+		locales: locales,
+	},
+	markdown: {
+		rehypePlugins:[[
+			rehypeExternalLinks, {
+				target: '_blank',
+				rel: ['nofollow', 'noreferrer'],
+			}
+		]]
+	}
+});

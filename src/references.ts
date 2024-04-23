5080b95159f006574857419b0fc92c34250ec446
Gerd Müller <gerdesque@posteo.org>
2024-04-23 09:15:40 +0200

    ✨ feat(app): adjust references to new structure

---
+++ src/references.ts
@@ -0,0 +1,52 @@
+export type ReferencesData = {
+  author: {
+    [key: string]: string
+  }
+  description: {
+    [key: string]: string
+  }
+}
+
+export const referencesData: ReferencesData[] = [
+  {
+    author: {
+      de: "Philippe Genêt, Projektkoordinator Coding da Vinci",
+      en: "Philippe Genêt, Project coordinator Coding da Vinci"
+    },
+    description: {
+      de: "Die Projekte von Anne Mühlich und Gerd Müller sind eine große Bereicherung für den Kultur-Hackathon Coding da Vinci. Mit jeder Teilnahme beweisen die beiden aufs Neue ihre Kreativität und ihren sensiblen Umgang mit offenen Kulturdaten und den dahinterstehenden Institutionen – das beweisen nicht zuletzt die Coding da Vinci-Stipendien, mit denen sie mehrfach ausgezeichnet wurden.",
+      en: "The projects of Anne Mühlich and Gerd Müller are a great enrichment for the Coding da Vinci cultural hackathon. With each participation, those two prove their creativity and their sensitive handling of open cultural data and the institutions behind it - as evidenced not least by the Coding da Vinci scholarships with which they have been awarded several times.",
+    },
+  },
+  {
+    author: {
+      de: "Maria Daldrup, Leiterin des Archivs der Arbeiterjugendbewegung",
+      en: "Maria Daldrup, Head of the Archive of the Worker's Youth Movement"
+      
+    },
+    description: {
+      de: "Anne Mühlich und Gerd Müller haben mit den Quellen des Archivs der Arbeiterjugendbewegung im Rahmen des Kulturhackathons „Coding da Vinci“ Fantastisches vollbracht: eine ästhetisch ansprechende, technisch einwandfreie und zielgruppengerechte Homepage zu den Kinderrepubliken der 1920er Jahre! Wir sind aber nicht nur überzeugt von der Kreativität und Professionalität des Teams, sondern auch von der unkomplizierten und konstruktiven Zusammenarbeit.",
+      en: "Anne Mühlich and Gerd Müller have achieved fantastic things with the sources of the Archive of the Workers' Youth Movement within the framework of the cultural hackathon 'Coding da Vinci': an aesthetically pleasing, technically flawless and target group-oriented homepage on the Children's Republics of the 1920s! But we are not only convinced by the creativity and professionalism of the team, but also by the uncomplicated and constructive cooperation.",
+    },
+  },
+  {
+    author: {
+      de: "Karsten Heck, Universität Göttingen / Forum Wissen",
+      en: "Karsten Heck, Göttingen University / Forum Wissen"
+    },
+    description: {
+      de: "Mit Plantala ist Anne Mühlich und Gerd Müller wunderbares gelungen: offene Kulturdaten in einer Software und ästhetischen Sprache zu präsentieren, die direkt ins Analoge zurückkoppelt. Große Spielfreude mit großem Mehrwert. Danke!",
+      en: "With Plantala, Anne Mühlich and Gerd Müller have accomplished something wonderful: presenting open cultural data in a software and aesthetic language that ties directly back into the analog. Great fun with great value. Thank you!",
+    },
+  },
+  {
+    author: {
+      de: "Liane Hellmund, freiberufliche Grafikdesignerin",
+      en: "Liane Hellmund, freelance graphic designer"
+    },
+    description: {
+      de: "Die Zusammenarbeit mit Anne Mühlich und Gerd Müller im Rahmen des Kultur-Hackathon Coding da Vinci Nieder.Rhein.Land 2021 war inspirierend und klappte reibungslos auch über die Entfernung hinweg. Dieses kollaborative Arbeiten war eine tolle Erfahrung und ich bin sehr stolz auf unser Ergebnis.",
+      en: "The collaboration with Anne Mühlich and Gerd Müller as part of the cultural hackathon Coding da Vinci Nieder.Rhein.Land 2021 was inspiring and worked smoothly even over distance. This collaborative work was a great experience and I am very proud of our result.",
+    },
+  },
+]

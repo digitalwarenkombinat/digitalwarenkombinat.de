d44160fa32c6c7571807e73daa6968fddd61c329
Gerd Müller <gerdesque@posteo.org>
2024-07-05 13:29:42 +0200

    ✨ feat(app): use icon component for SVG styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,35 @@
-import mdx from '@astrojs/mdx'
-import { defineConfig } from 'astro/config'
-import rehypeExternalLinks from 'rehype-external-links'
-import { site } from './src/consts'
-import { defaultLocale, locales } from './src/i18n/i18n'
-const sitemapLocales = Object.fromEntries(
-  locales.map((_, i) => [locales[i], locales[i]]),
-) // Create an object with keys and values based on locales
+import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+import { site } from './src/consts';
+import { defaultLocale, locales } from './src/i18n/i18n';
+const sitemapLocales = Object.fromEntries(locales.map((_, i) => [locales[i], locales[i]])); // Create an object with keys and values based on locales
 
-import sitemap from '@astrojs/sitemap'
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
-import tailwind from '@astrojs/tailwind'
-
+// https://astro.build/config
 export default defineConfig({
   site: site,
-  integrations: [
-    mdx(),
-    sitemap({
-      filter: (page) => page.secret !== true,
-      i18n: {
-        defaultLocale: defaultLocale,
-        locales: sitemapLocales,
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [mdx(), sitemap({
+    filter: page => page.secret !== true,
+    i18n: {
+      defaultLocale: defaultLocale,
+      locales: sitemapLocales
+    }
+  }), tailwind(), icon()],
   i18n: {
     defaultLocale: defaultLocale,
-    locales: locales,
+    locales: locales
   },
   markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: {
-            type: 'text',
-            value: ' 🔗',
-          },
-          target: '_blank',
-          rel: ['nofollow', 'noreferrer'],
-        },
-      ],
-    ],
-  },
-})
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      },
+      target: '_blank',
+    }]]
+  }
+});
